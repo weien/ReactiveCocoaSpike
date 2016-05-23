eb3308dbcc12e7483075dfa34dbe5d43076ed05f
Weien Wang <weienw@gmail.com>
2016-05-23 16:33:18 -0500

    [NEW] WWFoursquareService

---
+++ ReactiveCocoaSpike/ReactiveCocoaSpike/WWFoursquareService.swift
@@ -0,0 +1,13 @@
+//
+//  WWFoursquareService.swift
+//  ReactiveCocoaSpike
+//
+//  Created by Weien Wang on 5/23/16.
+//  Copyright © 2016 Weien Wang. All rights reserved.
+//
+
+import UIKit
+
+class WWFoursquareService: NSObject {
+
+}
